b886971cc6674da0a91a6228164b60fa65cce869
marcobusacca <marcobusacca01@gmail.com>
2023-11-13 16:43:01 +0100

    java-oop-shop completato

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Prodotto p = new Prodotto("prodotto", "descrizione", 111.25, 22);
+		
+		System.out.println(p);
+		
+		System.out.println("Prezzo senza IVA: " + p.getPrezzoBase());
+		
+		System.out.println("Prezzo comprensivo di IVA: " + p.getPrezzoPieno());
+		
+		System.out.println("Nome esteso: " + p.getNomeEsteso());
 	}
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,108 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	
+	Random rnd = new Random();
+	
+	
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private int iva;
+	
+	
+	public Prodotto(String nome, String descrizione, double prezzo, int iva) {
+		
+		setCodice();
+		setNome(nome);
+		setDescrizione(descrizione);
+		setPrezzo(prezzo);
+		setIva(iva);
+	}
+	
+	
+	// GETTER & SETTER: CODICE
+	public int getCodice() {
+		
+		return codice;
+	}
+	private void setCodice() {
+		
+		this.codice = rnd.nextInt(1, 1000);
+	}
+	
+	// GETTER & SETTER: NOME
+	public String getNome() {
+		
+		return nome;
+	}
+	public void setNome(String nome) {
+		
+		this.nome = nome;
+	}
+	
+	// GETTER & SETTER: DESCRIZIONE
+	public String getDescrizione() {
+		
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		
+		this.descrizione = descrizione;
+	}
+	
+	// GETTER & SETTER: PREZZO
+	public double getPrezzo() {
+		
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		
+		this.prezzo = prezzo;
+	}
+	
+	// GETTER & SETTER: IVA
+	public int getIva() {
+		
+		return iva;
+	}
+	public void setIva(int iva) {
+		
+		this.iva = iva;
+	}
+	
+	
+	public String getPrezzoBase() {
+		
+		return String.format("%.02f", getPrezzo()) + "€";
+	}
+	public String getPrezzoPieno() {
+		
+		return String.format("%.02f", getPrezzo() * (100 + iva) / 100) + "€";
+	}
+	public String getCodiceFormattato() {
+		
+		return String.format("%08d", getCodice());
+	}
+	public String getNomeEsteso() {
+		
+		return getCodiceFormattato() + "-" + getNome();
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+		return "--------------------\n"
+				+ "Prodotto: \n"
+				+ "Codice: " + getCodiceFormattato() + "\n"
+				+ "Nome: " + getNome() + "\n"
+				+ "Descrizione: " + getDescrizione() + "\n"
+				+ "Prezzo: " + getPrezzo() + "\n"
+				+ "Iva: " + getIva() + "\n"
+				+ "--------------------";
+	}
 }
